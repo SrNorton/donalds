add2e204c50b16476afc1109ba2d66ee056c3cf9
SrNorton <glauberfreestyle@gmail.com>
2023-12-12 23:36:52 -0300

    produtos adicionados

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -15,6 +15,7 @@ class Product {
    Product(icon: 'assets/images/donuts_cobertura.png', title: 'chocolate', price: 14.99),
    Product(icon: 'assets/images/donuts_docedeleite.png', title: 'Glazed', price: 14.99),
    Product(icon: 'assets/images/donuts_welcome.png', title: 'Homer', price: 14.99),
+   Product(icon: 'assets/images/chocolate.png', title: 'Granulado', price: 14.99),
 
    
    
@@ -24,6 +25,7 @@ class Product {
     Product(icon: 'assets/images/cafe_choco.png', title: 'Alpino', price: 19.99),
   Product(icon: 'assets/images/cafe_late.png', title: 'Au Lait', price: 19.99),
   Product(icon: 'assets/images/capuccino_camadas.png', title:'Macchiato 3', price: 19.99),
+  Product(icon: 'assets/images/cremegelado.png', title:'Creme Gelado', price: 19.99),
   ];
 
   
